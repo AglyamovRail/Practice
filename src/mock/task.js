c75e0fa72148c86d36bbb4bfb5586580b7112166
Rail <rick552rick@gmail.com>
2024-09-23 17:56:25 +0300

    added lab 3

---
+++ src/mock/task.js
@@ -0,0 +1,54 @@
+export const tasks = [
+    {
+        listname: 'Бэклог',
+        status: 'backlog',
+        tasks: [
+            {
+                name: 'Выучить JS',
+            },  
+            {
+                name: 'Выучить React',
+            },
+            {
+                name: 'Сделать домашку',
+            }
+        ]
+    },
+    {
+        listname: 'В процессе',
+        status: 'in-process',
+        tasks: [
+            {
+                name: 'Выпить смузи',
+            },  
+            {
+                name: 'Попить воды',
+            },
+            
+        ]
+    },
+    {
+        listname: 'Готово',
+        status: 'done',
+        tasks: [
+            {
+                name: 'Позвонить маме',
+            },  
+            {
+                name: 'Погладить кота',
+            }
+        ]
+    },
+    {
+        listname: 'Корзина',
+        status: 'basket',
+        tasks: [
+            {
+                name: 'Сходить погулять',
+            },  
+            {
+                name: 'Прочитать Войну и Мир',
+            }
+        ]
+    }
+]
